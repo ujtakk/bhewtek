3a0d5f63a35da0cd90ca7f7569f097230d2046df
Takayuki Ujiie <takau@easter.kuee.kyoto-u.ac.jp>
2016-04-15 01:14:21 +0900

    sram -> modeled ones

---
+++ rtl/parameters.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+# ERB parameter
+$project = '/home/work/takau/bhewtek'
+$core = 4
+$tech = nil # rohm
+
+# Verilog parameter
+$dwidth = 16
+$insize = 12
+$fsize = 5
+$psize = 2
+$n_f1 = 20
+$n_f2 = 50
+$outsize = 10
+$wsize = 13
+$ifmsize = 9
+$faccum = 7
+$paccum = 8
+$lwidth = 8
+$step = 10
+

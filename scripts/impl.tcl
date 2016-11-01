e36e7579118b4cbc84da0535a990653927f921e6
Takayuki Ujiie <takau@easter.kuee.kyoto-u.ac.jp>
2016-11-01 20:45:55 +0900

    mod

--- scripts/impl.tcl
+++ scripts/impl.tcl
@@ -10,25 +10,49 @@
 # Journal file: /ram/home/takau/vivado.jou
 #-----------------------------------------------------------
 
+set top_dir [exec git rev-parse --show-toplevel]
+set board zedboard
+
+set xpr           $top_dir/$board/$board.xpr
+set srcs          $top_dir/$board/$board.srcs
+set ip_user_files $top_dir/$board/$board.ip_user_files
+set runs          $top_dir/$board/$board.runs
+set sdk           $top_dir/$board/$board.sdk
+
 # start_gui
-open_project /ram/home/work/takau/bhewtek/zedboard/zedboard.xpr
+open_project $xpr
 update_compile_order -fileset sources_1
-open_bd_design {/ram/home/work/takau/bhewtek/zedboard/zedboard.srcs/sources_1/bd/cnn_system/cnn_system.bd}
+open_bd_design {$srcs/sources_1/bd/cnn_system/cnn_system.bd}
+
 update_ip_catalog -rebuild -scan_changes
 report_ip_status -name ip_status
-export_ip_user_files -of_objects [get_ips  cnn_system_copro_0_0] -no_script -reset -quiet
-upgrade_ip -vlnv ac.jp:user:copro:1.0 [get_ips  cnn_system_copro_0_0] -log ip_upgrade.log
-generate_target all [get_files  /ram/home/work/takau/bhewtek/zedboard/zedboard.srcs/sources_1/bd/cnn_system/cnn_system.bd]
-export_ip_user_files -of_objects [get_files /ram/home/work/takau/bhewtek/zedboard/zedboard.srcs/sources_1/bd/cnn_system/cnn_system.bd] -no_script -force -quiet
-export_simulation -of_objects [get_files /ram/home/work/takau/bhewtek/zedboard/zedboard.srcs/sources_1/bd/cnn_system/cnn_system.bd] -directory /ram/home/work/takau/bhewtek/zedboard/zedboard.ip_user_files/sim_scripts -ip_user_files_dir /ram/home/work/takau/bhewtek/zedboard/zedboard.ip_user_files -ipstatic_source_dir /ram/home/work/takau/bhewtek/zedboard/zedboard.ip_user_files/ipstatic -force -quiet
-report_ip_status -name ip_status 
+export_ip_user_files \
+  -of_objects [get_ips  cnn_system_copro_0_0] \
+  -no_script -reset -quiet
+upgrade_ip \
+  -vlnv ac.jp:user:copro:1.0 [get_ips  cnn_system_copro_0_0] \
+  -log ip_upgrade.log
+generate_target all [get_files  $srcs/sources_1/bd/cnn_system/cnn_system.bd]
+export_ip_user_files \
+  -of_objects [get_files $srcs/sources_1/bd/cnn_system/cnn_system.bd] \
+  -no_script -force -quiet
+export_simulation \
+  -of_objects [get_files $srcs/sources_1/bd/cnn_system/cnn_system.bd] \
+  -directory $ip_user_files/sim_scripts \
+  -ip_user_files_dir $ip_user_files \
+  -ipstatic_source_dir $ip_user_files/ipstatic \
+  -force -quiet
+report_ip_status -name ip_status
+
 reset_run synth_1
 launch_runs synth_1 -jobs 2
 wait_on_run synth_1
+
 launch_runs impl_1 -jobs 2
 wait_on_run impl_1
 launch_runs impl_1 -to_step write_bitstream -jobs 2
 wait_on_run impl_1
-file copy -force /ram/home/work/takau/bhewtek/zedboard/zedboard.runs/impl_1/cnn_system_wrapper.sysdef /ram/home/work/takau/bhewtek/zedboard/zedboard.sdk/cnn_system_wrapper.hdf
 
-# launch_sdk -workspace /ram/home/work/takau/bhewtek/zedboard/zedboard.sdk -hwspec /ram/home/work/takau/bhewtek/zedboard/zedboard.sdk/cnn_system_wrapper.hdf
+file copy -force $runs/impl_1/cnn_system_wrapper.sysdef $sdk/cnn_system_wrapper.hdf
+
+# launch_sdk -workspace $sdk -hwspec $sdk/cnn_system_wrapper.hdf

0bfc5153fc0b1d653623a6330335cc602b685b91
Takayuki Ujiie <takau@easter.kuee.kyoto-u.ac.jp>
2016-11-08 21:24:50 +0900

    mod

--- zedboard/zedboard.runs/impl_1/runme.sh
+++ zedboard/zedboard.runs/impl_1/runme.sh
@@ -37,7 +37,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log cnn_system_wrapper.vdi -applog -m64 -messageDb vivado.pb -mode batch -source cnn_system_wrapper.tcl -notrace
 
 
